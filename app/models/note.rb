2fdd4d9f3ec12678edb6eebe1dedb539b06a137f
My Name <tera@teradale.com>
2015-03-16 13:56:39 +0000

    added preview view

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,5 +5,9 @@ class Note < ActiveRecord::Base
   validates :content, presence: true
 
   scope :sorted, lambda { order('created_at DESC') }
+  
+  def preview
+    self.content.slice(0..99) << " ... (readmore)"
+  end 
 
 end
